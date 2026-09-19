d7e54fec8bdd66b74c23179b82b36dec0bb05546
seigo <v2v8z9tn9s@privaterelay.appleid.com>
2026-09-19 20:27:14 +0900

    Delete cofe_app directory

--- cofe_app/README.md
+++
@@ -1,1 +0,0 @@
-# cofe_app
--- cofe_app/cofe_app/cofe_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from polls import views
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('cofe_home/', include('polls.urls')),
-    path('', views.home, name='home'),
-]
